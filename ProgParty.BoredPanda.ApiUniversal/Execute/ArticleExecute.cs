3751bd559726f6c59a43363ce409d11d0ace1d96
Jens den Braber <jens.denbraber@gmail.com>
2016-04-25 21:43:22 +0200

    projects for universal windows added

---
+++ ProgParty.BoredPanda.ApiUniversal/Execute/ArticleExecute.cs
@@ -0,0 +1,27 @@
+﻿using ProgParty.BoredPanda.ApiUniversal.Result;
+using ProgParty.BoredPanda.ApiUniversal.Scrape;
+using System;
+using System.Collections.Generic;
+
+namespace ProgParty.BoredPanda.ApiUniversal.Execute
+{
+    public class ArticleExecute
+    {
+        public Parameter.ArticleParameter Parameters = new Parameter.ArticleParameter();
+
+        public ArticleResult Result;
+
+        public bool Execute()
+        {
+            try
+            {
+                Result = new ArticleScrape(Parameters).Execute();
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ProgParty.BoredPanda.ApiUniversal/Execute/OverviewExecute.cs
@@ -0,0 +1,26 @@
+﻿using ProgParty.BoredPanda.ApiUniversal.Result;
+using ProgParty.BoredPanda.ApiUniversal.Scrape;
+using System.Collections.Generic;
+
+namespace ProgParty.BoredPanda.ApiUniversal.Execute
+{
+    public class OverviewExecute
+    {
+        public Parameter.OverviewParameter Parameters = new Parameter.OverviewParameter();
+
+        public List<OverviewResult> Result;
+
+        public bool Execute()
+        {
+            try
+            {
+                Result = new OverviewScrape(Parameters).Execute();
+                return true;
+            }
+            catch(System.Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
